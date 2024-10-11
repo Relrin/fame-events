b6d5c150e49b924db4dde1c69b64b98100e0aa7f
Valeryi Savich <relrin78@gmail.com>
2024-10-11 09:55:44 +0200

    Added event scheduler

--- pkg/event_scheduler.go
+++ pkg/event_scheduler.go
@@ -1,19 +1,33 @@
 package pkg
 
-import "time"
-
 type EventScheduler struct {
 	Name        string
-	CreatedAt   time.Time
-	StartedAt   time.Time
 	GroupStages []*GroupStage
 
 	// Turn into config?
-	// TiebreakResolver *TiebreakResolver
+	TiebreakResolver *TiebreakResolver
 	// PlayOffOptimizer
 	// GroupStageOptimizer
 }
 
-func (es *EventScheduler) HasTiebreak(gs *GroupStage) bool {
-	return false
+func NewEventScheduler() *EventScheduler {
+	return &EventScheduler{}
+}
+
+// WithGroupStages sets a list of group stages to handle for the event
+func (es *EventScheduler) WithGroupStages(groupStages []*GroupStage) *EventScheduler {
+	es.GroupStages = groupStages
+	return es
+}
+
+// WithTiebreakResolver sets a new tiebreak resolver for the event
+func (es *EventScheduler) WithTiebreakResolver(tiebreakResolver *TiebreakResolver) *EventScheduler {
+	es.TiebreakResolver = tiebreakResolver
+	return es
+}
+
+func (es *EventScheduler) Run() {
+	// run an infinite loop where
+	// 1. We pass through all group stages & schedule a next match whenever is possible within the group (it includes tiebreakers)
+	// 2. Once we finished with all the matches, start a playoff by advancing N teams further. Also schedule a separate event for them
 }
