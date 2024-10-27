7bc22a08ac6d04dacf53b2f244d0f6e8b6a347d3
Valeryi Savich <relrin78@gmail.com>
2024-10-27 14:58:23 +0100

    Added proto for event service

--- pkg/team.go
+++ pkg/team.go
@@ -2,6 +2,7 @@ package pkg
 
 type Team struct {
 	Name       string        // Displayed name in the event
+	Id         string        // A unique id assigned to the team for the entire tournament
 	BrandIndex uint32        // The team brand within the event
 	Color      uint32        // Assigned color through the entire event
 	Players    []*PlayerInfo // List of players assigned to the team
